9a8367862392877988672eb9bfc2b19ca26d0f43
Suyash Nigam <71869185+Suyash2711@users.noreply.github.com>
2022-06-10 14:59:12 +0530

    added function

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -9,10 +9,39 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick = ()=>{
+        // console.log("Lower Case was Clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleclearClick = ()=>{
+        // console.log("Lower Case was Clicked");
+        let newText = "";
+        setText(newText);
+    }
+    const handleinverseClick = ()=>{
+        // console.log("Lower Case was Clicked");
+        var chars = text.toUpperCase().split("");
+        for (var i = 0; i < chars.length; i += 2) {
+            chars[i] = chars[i].toLowerCase();
+          }
+        let newText = chars.join("");
+        setText(newText);
+    }
+    const handlealtClick = ()=>{
+        // console.log("Alternate Case was Clicked");
+        var chars = text.toLowerCase().split("");
+        for (var i = 0; i < chars.length; i += 2) {
+            chars[i] = chars[i].toUpperCase();
+          }
+        let newText = chars.join("");
+        setText(newText);
+    }
     const handleOnChange = (event)=>{
         // console.log("On Change");
         setText(event.target.value);
     }
+    
     // text = "New Text"; //Wrong way to change the state
     // setText("New Text"); //Correct way to change the state
     return (
@@ -21,11 +50,18 @@ export default function TextForm(props) {
         {/* <label for="mybox" className="form-label">{props.heading}</label> */}
         <h1>{props.heading}</h1>
         <textarea className="form-control" value = {text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handlealtClick}>AlTeRnAtInG cAsE</button>
+        <button className="btn btn-primary mx-2" onClick={handleclearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleinverseClick}>iNvErSe CaSe</button>
     </div>
     <div className="container my-2">
         <h3>Your Text Summary</h3>
         <p>{text.split(" ").length} words, {text.length} characters</p>
+        <b><p>{0.008 * text.split(" ").length} Minutes Read</p></b>
+        <h3>Preview</h3>
+        <p>{text}</p>
     </div>
     </>
   )
