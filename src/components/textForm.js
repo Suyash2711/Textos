4ce9b40e0bd81881c164067c85e97ad9b92796f5
Suyash Nigam <71869185+Suyash2711@users.noreply.github.com>
2022-06-10 14:23:02 +0530

    worde counter added

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -16,11 +16,17 @@ export default function TextForm(props) {
     // text = "New Text"; //Wrong way to change the state
     // setText("New Text"); //Correct way to change the state
     return (
-    <div className="mb-3">
+        <>
+    <div className="container my-3">
         {/* <label for="mybox" className="form-label">{props.heading}</label> */}
         <h1>{props.heading}</h1>
         <textarea className="form-control" value = {text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
     </div>
+    <div className="container my-2">
+        <h3>Your Text Summary</h3>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+    </div>
+    </>
   )
 }
